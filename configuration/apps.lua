1e114181b00d8cf8c7e28535e1f976c21ff51cb7
Sanket Debnath <sanketdebnath24@gmail.com>
2020-03-17 16:00:00 +0530

    Utility Changes

--- configuration/apps.lua
+++ configuration/apps.lua
@@ -6,38 +6,38 @@ local get_dpi = require('beautiful').xresources.get_dpi
 local rofi_command = 'env /usr/bin/rofi -dpi ' .. get_dpi() .. ' -width ' .. with_dpi(400) .. ' -show drun -theme ' .. filesystem.get_configuration_dir() .. '/configuration/rofi.rasi -run-command "/bin/bash -c -i \'shopt -s expand_aliases; {cmd}\'"'
 
 return {
-  -- List of apps to start by default on some actions
-  default = {
-    terminal = 'kitty',
-    rofi = rofi_command,
-    lock = 'blurlock',
-    quake = 'kitty',
-    screenshot = '~/.config/awesome/configuration/utils/screenshot -m',
-    region_screenshot = '~/.config/awesome/configuration/utils/screenshot -r',
-    delayed_screenshot = 'sleep 10 ; ~/.config/awesome/configuration/utils/screenshot -r',
-    
-    -- Editing these also edits the default program
-    -- associated with each tag/workspace
-    browser = 'chromium',
-    term = 'kitty',
-    files = 'pcmanfm',
-    editor = 'kitty -e vim',
-  },
-  -- List of apps to start once on start-up
-  run_on_start_up = {
-    'compton --config ' .. filesystem.get_configuration_dir() .. '/configuration/compton.conf',
-    'nm-applet', --wifi
-    --'blueberry-tray', -- Bluetooth tray icon
-    'xfce4-power-manager', -- Power manager
-    'ibus-daemon --xim', -- Ibus daemon for keyboard
-    'scream -u -p 4011 -i virbr1', -- scream audio sink
-    'numlockx on', -- enable numlock
-    '/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 & eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg)', -- credential manager
-    --KDE '/usr/lib/x86_64-linux-gnu/libexec/polkit-kde-authentication-agent-1 & eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg)', -- credential manager
-    -- MATE'/usr/lib/mate-polkit/polkit-mate-authentication-agent-1 & eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg)', -- credential manager
-    'pamac-tray',
-    -- Add applications that need to be killed between reloads
-    -- to avoid multipled instances, inside the awspawn script
-    '~/.config/awesome/configuration/awspawn' -- Spawn "dirty" apps that can linger between sessions
-  }
+    -- List of apps to start by default on some actions
+    default = {
+        terminal = 'kitty',
+        rofi = rofi_command,
+        lock = 'blurlock',
+        quake = 'kitty',
+        screenshot = '~/.config/awesome/configuration/utils/screenshot -m',
+        region_screenshot = '~/.config/awesome/configuration/utils/screenshot -r',
+        delayed_screenshot = 'sleep 10 ; ~/.config/awesome/configuration/utils/screenshot -r',
+
+        -- Editing these also edits the default program
+        -- associated with each tag/workspace
+        browser = 'chromium',
+        term = 'kitty',
+        files = 'pcmanfm',
+        editor = 'kitty -e vim',
+    },
+    -- List of apps to start once on start-up
+    run_on_start_up = {
+        'compton --config ' .. filesystem.get_configuration_dir() .. '/configuration/compton.conf',
+        'nm-applet', --wifi
+        --'blueberry-tray', -- Bluetooth tray icon
+        'xfce4-power-manager', -- Power manager
+        'ibus-daemon --xim', -- Ibus daemon for keyboard
+        'scream -u -p 4011 -i virbr1', -- scream audio sink
+        'numlockx on', -- enable numlock
+        '/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 & eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg)', -- credential manager
+        --KDE '/usr/lib/x86_64-linux-gnu/libexec/polkit-kde-authentication-agent-1 & eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg)', -- credential manager
+        -- MATE'/usr/lib/mate-polkit/polkit-mate-authentication-agent-1 & eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg)', -- credential manager
+        'pamac-tray',
+        -- Add applications that need to be killed between reloads
+        -- to avoid multipled instances, inside the awspawn script
+        '~/.config/awesome/configuration/awspawn' -- Spawn "dirty" apps that can linger between sessions
+    }
 }
